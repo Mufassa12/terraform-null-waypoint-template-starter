cb3925cfb564deb380ca0664ad40fd0ed49b18d2
Shaun S <shaun.stuart@gmail.com>
2023-08-31 14:01:55 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,4 +3,5 @@
 variable "waypoint_project" {
   description = "Name of the waypoint project, to be used in all resource names."
   type        = string
+  value       = "7a9f1003-9434-4cf6-bc28-b466b5185a6a"
 }
